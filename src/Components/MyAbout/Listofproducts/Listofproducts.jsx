965e43410a0a01e016f0afeb3afc9e1883806f6c
Dinesh <dineshkit15@gmail.com>
2025-09-06 19:16:02 +0530

    update %

--- src/Components/MyAbout/Listofproducts/Listofproducts.jsx
+++ src/Components/MyAbout/Listofproducts/Listofproducts.jsx
@@ -150,7 +150,7 @@ const Listofproducts = () => {
           </table>
 
           <div className="discount-container">
-            <h2>65% Discount</h2>
+            <h2>75% Discount</h2>
           </div>
 
           <table className="product_table">
